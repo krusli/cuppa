61b68d029651faf302cc1e9a81041fc6bb3070ad
Kenneth <14309096+krusli@users.noreply.github.com>
2019-03-04 09:23:09 +0700

    CORS for groups

--- cuppa-groups/app.js
+++ cuppa-groups/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const Group = require('./models/group');
 
 const app = express();
 app.use(helmet());
+app.use(cors());
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
