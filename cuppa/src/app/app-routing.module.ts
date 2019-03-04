b1d67b1886412a832948995487c4797fd589b82d
Kenneth <14309096+krusli@users.noreply.github.com>
2019-03-04 22:18:27 +0700

    Update group view link

--- cuppa/src/app/app-routing.module.ts
+++ cuppa/src/app/app-routing.module.ts
@@ -27,7 +27,7 @@ const routes: Routes = [
         component: NewGroupComponent
       },
       {
-        path: ':groupId',
+        path: 'view/:groupId',
         component: GroupPageComponent
       },
       {
