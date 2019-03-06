f14c603822d3b9da3f13094455982f063bc81a33
Kenneth <14309096+krusli@users.noreply.github.com>
2019-03-06 08:45:53 +0700

    Add descriptive comment for Activity model

--- cuppa-activity/models/activity.js
+++ cuppa-activity/models/activity.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+/**
+ * An Action: any mutation in the data. Each Service is responsible for notifying
+ * the Activity Microservice about new actions.
+ * 
+ * An Activity is represented in the passive voice
+ * (Subject was Action-ed by the User)
+ * 
+ * Illustrative example:
+ * （１）　ポリッジが誰かに食べられた！
+ * - The porridge was eaten by somebody!
+ */
 const activitySchema = new Schema({
   user: { type: mongoose.Types.ObjectId, required: true },
   // group: mongoose.Types.ObjectId,
