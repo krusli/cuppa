9a6cbb843d2d472a194b568bff2430f78ea1b08c
Kenneth <14309096+krusli@users.noreply.github.com>
2019-05-08 16:01:09 +0700

    Add resiliency: sometimes MongoDB won't be ready when we are

--- cuppa-groups/app.js
+++ cuppa-groups/app.js
@@ -16,8 +16,40 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://mongo:27017/cuppa-users', { useNewUrlParser: true, reconnectTries: 30 })
-    .then(() => console.log('Connected to MongoDB'), err => console.log(err));
+function createConnection(dbURL, options) {
+    var db = mongoose.createConnection(dbURL, options);
+
+    db.on('error', function (err) {
+        // If first connect fails because mongod is down, try again later.
+        // This is only needed for first connect, not for runtime reconnects.
+        // See: https://github.com/Automattic/mongoose/issues/5169
+        if (err.message && err.message.match(/failed to connect to server .* on first connect/)) {
+            console.log(new Date(), String(err));
+
+            // Wait for a bit, then try to connect again
+            setTimeout(function () {
+                console.log("Retrying first connect...");
+                db.openUri(dbURL).catch(() => { });
+                // Why the empty catch?
+                // Well, errors thrown by db.open() will also be passed to .on('error'),
+                // so we can handle them there, no need to log anything in the catch here.
+                // But we still need this empty catch to avoid unhandled rejections.
+            }, 20 * 1000);
+        } else {
+            // Some other error occurred.  Log it.
+            console.error(new Date(), String(err));
+        }
+    });
+
+    db.once('open', function () {
+        console.log("Connection to db established.");
+    });
+
+    return db;
+}
+
+// Use it like
+var db = createConnection('mongodb://mongo:27017/cuppa-users', { useNewUrlParser: true });
 
 const groupsService = new GroupsService(UsersService, Group);
 
