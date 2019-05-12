bce76d2a4ce490ff536d0d1d0015dada058b3367
Kenneth <14309096+krusli@users.noreply.github.com>
2019-05-12 22:09:02 +0700

    Load Group from Groups Store in Groups Page

--- cuppa/src/app/groups/group-page/group-page.component.ts
+++ cuppa/src/app/groups/group-page/group-page.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { Group, GroupsAndUsers } from 'src/app/models/Group';
+import * as fromRoot from 'src/app/store/reducers';
+
+
+import { Group } from 'src/app/models/Group';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { NavItem, NavItemImpl } from 'src/app/common/tab-bar/tab-bar.component';
 import { Observable } from 'rxjs';
+import { select, Store } from '@ngrx/store';
+import { GroupsSelectors } from 'src/app/store/reducers/groups.reducer';
 
 @Component({
   selector: 'app-group-page',
@@ -17,16 +22,22 @@ export class GroupPageComponent implements OnInit {
 
   navItems: NavItem[];
 
-  groupsAndUsers: Observable<GroupsAndUsers>;
-  group: Group;
+  // group: Group;
+  group$: Observable<Group>;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private store: Store<fromRoot.State>) {
   }
 
   ngOnInit() {
     /* Assumption: Groups store already populated by parent Groups Component (routing parent) */
     const groupId = this.route.snapshot.paramMap.get('groupId');
     this.updateNavItems(groupId);
+
+    this.group$ = this.store.pipe(
+      select('groups'),
+      select(GroupsSelectors.selectEntities),
+      select(x => x[groupId])
+    )
   }
 
   updateNavItems(groupId: string) {
