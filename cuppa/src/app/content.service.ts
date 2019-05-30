3550128d6e2cd3f8a5921c9df3c0778a74894bb8
Kenneth <14309096+krusli@users.noreply.github.com>
2019-05-30 19:41:16 +0700

    Use environment.baseUrl so we can change URL when deployed to prod

--- cuppa/src/app/content.service.ts
+++ cuppa/src/app/content.service.ts
@@ -3,17 +3,20 @@ import { HttpClient } from '@angular/common/http';
 import { AuthService } from './auth.service';
 
 import Consts from 'src/app/consts';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
 
+  baseUrl = environment.baseUrl;
+
   constructor(private http: HttpClient, private authService: AuthService) { }
 
   getFeaturedCommunities() {
     const headers = this.authService.getHeaders();
-    return this.http.get(`${Consts.BASE_URL}:3003/communities/featured`, { headers });
+    return this.http.get(`${this.baseUrl}:3003/communities/featured`, { headers });
   }
 
 }
--- cuppa/src/app/groups.service.ts
+++ cuppa/src/app/groups.service.ts
@@ -4,27 +4,30 @@ import { AuthService } from './auth.service';
 import { GroupDTO } from './models/Group';
 
 import Consts from 'src/app/consts';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupsService {
 
+  baseUrl = environment.baseUrl;
+
   constructor(private http: HttpClient, private authService: AuthService) { }
 
   getGroups() {
     // get auth header
     const headers = this.authService.getHeaders();
-    return this.http.get(`${Consts.BASE_URL}:3003/groups`, { headers });
+    return this.http.get(`${this.baseUrl}:3003/groups`, { headers });
   }
 
   getGroup(groupId: string) {
     const headers = this.authService.getHeaders();
-    return this.http.get(`${Consts.BASE_URL}:3003/groups/${groupId}`, { headers });
+    return this.http.get(`${this.baseUrl}:3003/groups/${groupId}`, { headers });
   }
 
   newGroup(group: GroupDTO) {
     const headers = this.authService.getHeaders();
-    return this.http.post(`${Consts.BASE_URL}:3001/groups`, group, { headers });
+    return this.http.post(`${this.baseUrl}:3001/groups`, group, { headers });
   }
 }
